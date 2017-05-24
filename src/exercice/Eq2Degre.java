5a6502de99c91fd3aba49c79f4499378764ddf56
StraetenMax <maxstraeten@yahoo.com>
2017-05-24 16:37:32 +0200

    Eq2Degre méthodes

--- src/exercice/Eq2Degre.java
+++ src/exercice/Eq2Degre.java
@@ -15,12 +15,42 @@ public class Eq2Degre {
 		b = 0.00;
 		c = 0.00;
 	}
-	public Eq2Degre(double a, double b, double c){
-		this.a = a;
-		this.b = b;
-		this.c = c;
+	public Eq2Degre(double x1, double x2, double x3){
+		a = x1;
+		b = x2;
+		c = x3;
 	}
 	
-	
-	
-}
+	//accesseurs et mutateurs
+	public double getA(){
+		return a;
+	}
+	public double getB(){
+		return b;
+	}
+	public double getC(){
+		return c;
+	}
+	public void setA(double a){
+		this.a = a;
+	}
+	public void setB(double b){
+		this.b = b;
+	}
+	public void setC(double c){
+		this.c = c;
+	}
+	//méthodes
+	public void afficheDiscriminant(){
+		delta = b*b - 4.0*a*c;
+		System.out.println(delta);
+	}
+	public void resoudre(){
+		r1 = (-b - Math.sqrt(delta)) /(2.0*a);
+		r2 = (-b + Math.sqrt(delta))/(2.0*a);
+	}
+	public void afficheSolutions(){
+		System.out.println("La première racine est " + r1);
+		System.out.println("La deuxième racine est " + r2);
+	}
+	}
