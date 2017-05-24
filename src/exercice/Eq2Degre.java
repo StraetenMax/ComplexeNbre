8077c968cee9e6e92fab208edaa2c23ec438ec6c
StraetenMax <maxstraeten@yahoo.com>
2017-05-24 16:16:56 +0200

    class Eq2Degre

---
+++ src/exercice/Eq2Degre.java
@@ -0,0 +1,26 @@
+package exercice;
+
+public class Eq2Degre {
+	//propriétés
+	private double r1;
+	private double r2;
+	private double delta;
+	private double a;
+	private double b;
+	private double c;
+	
+	//constructeur 
+	public Eq2Degre(){
+		a = 0.00;
+		b = 0.00;
+		c = 0.00;
+	}
+	public Eq2Degre(double a, double b, double c){
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	
+	
+}
