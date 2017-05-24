b3d13c26c0bae540a3676b46bcbe56476b368ef6
StraetenMax <maxstraeten@yahoo.com>
2017-05-24 13:58:37 +0200

    Création ClassComplexe

--- src/exercice/ClassComplexe.java
+++ src/exercice/ClassComplexe.java
@@ -2,4 +2,34 @@ package exercice;
 
 public class ClassComplexe {
 	//propriétés
+	private int reel;
+	private int img;
+	
+	//constructeurs vide et initialisé
+	public ClassComplexe(){
+		reel = 0;
+		img = 0;
+	}
+	public ClassComplexe(int reel, int img){
+		this.reel = reel;
+		this.img = img;
+	}
+	
+	//accesseurs et mutateurs
+	public int getReel(){
+		return reel;
+	}
+	public void setReel(int reel){
+		this.reel=reel;
+	}
+	public int getImg(){
+		return img;
+	}
+	public void setImg(int img){
+		this.img = img;
+	}
+	
+	//méthodes
+	
 }
+
